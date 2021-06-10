949380bc93af341c2355687b9b2b4f1333f44c0f
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-06-10 19:59:31 -0400

    scrambled words in sentence

--- src/App.js
+++ src/App.js
@@ -22,7 +22,61 @@ function App() {
         }
     };
 
-    return <div className="App">{sentence && <h1>{sentence}</h1>}</div>;
+    const scrambleSentence = (sentence) => {
+        // break sentence into words
+        const words = sentence.split(" ");
+
+        // new sentence
+        let newSentence = [];
+
+        // do something for each word
+        words.forEach((word) => {
+            // if the word is bigger than 2 characters, shuffle it
+            if (word.length > 2) {
+                // break the word into characters
+                word = word.split("");
+
+                // take the first and last letters out since they won't be scrambled
+                let firstLetter = word.shift();
+                let lastLetter = word.pop();
+
+                // create a new scrambled word and give it the first fixed letter
+                let newWord = [];
+                newWord.push(firstLetter);
+
+                // while we still have letters in remaining in the word randomly remove them and append to new word
+                do {
+                    let i = Math.floor(Math.random() * word.length);
+                    let letter = word.splice(i, 1);
+                    newWord.push(letter[0]);
+                } while (word.length > 0);
+
+                // add the last letter back
+                newWord.push(lastLetter);
+
+                // convert mixed letters back into a word
+                newWord = newWord.join("");
+
+                // add scrambled word to sentence
+                newSentence.push(newWord);
+            } else {
+                // put unscrambled word into sentence
+                newSentence.push(word);
+            }
+        });
+
+        // convert new sentence array to string and return
+        newSentence = newSentence.join(" ");
+        return newSentence;
+    };
+
+    return (
+        <div className="App">
+            {sentence && <p id="scrambled-word">{sentence}</p>}
+
+            {sentence && scrambleSentence(sentence)}
+        </div>
+    );
 }
 
 export default App;
