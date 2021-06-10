acde239a798ba34017f09c4560d68563efd65492
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-06-10 19:11:27 -0400

    fetch sentence with axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Hello</h1>
-        </div>
-    );
+    const [sentence, setSentence] = useState();
+
+    useEffect(() => {
+        fetchSentence();
+    }, []);
+
+    const fetchSentence = async () => {
+        try {
+            await axios
+                .get(`https://api.hatchways.io/assessment/sentences/1`)
+                .then((res) => {
+                    console.log(res);
+                    setSentence(res.data.data.sentence);
+                });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return <div className="App">{sentence && <h1>{sentence}</h1>}</div>;
 }
 
 export default App;
